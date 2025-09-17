3d2f7880534fd2d0dabc17b0802c556e66f7a134
“Parth <parthpatel.bizinsights@gmail.com>
2025-09-17 17:12:00 +0530

    changing port

--- server/index.ts
+++ server/index.ts
@@ -62,11 +62,14 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '3001', 10);
-  server.listen({
+server.listen(
+  {
     port,
-    host: "127.0.0.1",
-    
-  }, () => {
+    host: "0.0.0.0",
+  },
+  () => {
     log(`serving on port ${port}`);
-  });
+  }
+);
+
 })();
